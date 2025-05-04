225193eff60628fcba9fd91d5a4920e9da22221c
faqihhidayah876 <136100051+faqihhidayah876@users.noreply.github.com>
2025-05-04 22:39:26 +0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,9 @@
+document.getElementById('tombol')
+.addEventListener('click', function() {
+    alert('Artikel ini belum tersedia')
+});
+
+document.getElementById('button')
+.addEventListener('click', function() {
+    alert('Artikel ini belum tersedia')
+});
